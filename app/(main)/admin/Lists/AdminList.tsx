0ffb6edef22d349ff9dc9623e09314980b61ba2a
Guilherme Lira <guilherme.liratsouza@gmail.com>
2025-11-29 10:20:49 -0300

    feat(admin): iniciada a seção para listar/deletar/atualizar

---
+++ app/(main)/admin/Lists/AdminList.tsx
@@ -0,0 +1,98 @@
+'use client'
+
+import { useState } from "react"
+
+import TagList from "./TagList"
+import ProdutoList from "./ProdutoList"
+import UserList from "./UserList"
+import AddressList from "./AddressList"
+import BrechoList from "./BrechoList"
+
+import AdminNavInput from "../AdminNavInput"
+
+
+export default function AdminList(){
+    const [isUserListOpen, setUserListOpen] = useState(false)    
+    const [isBrechoListOpen, setBrechoListOpen] = useState(false)    
+    const [isProdutoListOpen, setProdutoListOpen] = useState(false)    
+    const [isTagListOpen, setTagListOpen] = useState(false)    
+    const [isAddressListOpen, setAddressListOpen] = useState(false)    
+    
+    function handleAddressListOpen(){
+        setBrechoListOpen(false)
+        setProdutoListOpen(false)
+        setTagListOpen(false)
+        setUserListOpen(false)
+        setAddressListOpen(true)
+    }
+    function handleBrechoListOpen(){
+        setAddressListOpen(false)
+        setProdutoListOpen(false)
+        setTagListOpen(false)
+        setUserListOpen(false)
+        setBrechoListOpen(true)
+    }
+    function handleProdutoListOpen(){
+        setAddressListOpen(false)
+        setBrechoListOpen(false)
+        setTagListOpen(false)
+        setUserListOpen(false)
+        setProdutoListOpen(true)
+    }
+    function handleTagListOpen(){
+        setAddressListOpen(false)
+        setBrechoListOpen(false)
+        setProdutoListOpen(false)
+        setUserListOpen(false)
+        setTagListOpen(true)
+    }
+    function handleUserListOpen(){
+        setAddressListOpen(false)
+        setBrechoListOpen(false)
+        setProdutoListOpen(false)
+        setTagListOpen(false)
+        setUserListOpen(true)
+    }
+    return(
+        <>
+            <nav className="absolute flex justify-center top-20 left-1/2 -translate-1/2 w-full gap-10">
+                <button onClick={handleUserListOpen}>
+                    <AdminNavInput grupo="list">User</AdminNavInput>
+                </button>
+                    
+                <button onClick={handleBrechoListOpen}>
+                    <AdminNavInput grupo="list">Brecho</AdminNavInput>
+                </button>
+                    
+                <button onClick={handleProdutoListOpen}>
+                    <AdminNavInput grupo="list">Produto</AdminNavInput>
+                </button>
+                    
+                <button onClick={handleTagListOpen}>
+                    <AdminNavInput grupo="list">Tag</AdminNavInput>
+                </button>
+                    
+                <button onClick={handleAddressListOpen}>
+                    <AdminNavInput grupo="list">Address</AdminNavInput>
+                </button>
+            </nav>
+            <main className="pt-15 w-2/4">
+                {isAddressListOpen && (
+                    <AddressList />
+                )}
+                {isBrechoListOpen && (
+                    <BrechoList />
+                )}
+                {isProdutoListOpen && (
+                    <ProdutoList />
+                )}
+                {isTagListOpen && (
+                    <TagList />
+                )}
+                {isUserListOpen && (
+                    <UserList />
+                )}
+            </main>
+        </>    
+    )
+}
---
+++ app/(main)/admin/Lists/TagList.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { Tag } from "@/app/types/Tag";
+import { useEffect, useState } from "react";
+
+
+
+export default function TagList(){
+    function deleteTag(pk: number){
+        fetch(`http://localhost:8000/tags/${pk}/`, {
+            method: "DELETE"
+        })
+        .then(res => {
+            if(!res.ok) throw new Error("Tag não encontrada");
+            return res.json
+        })
+        .then(data => {
+            setTags(prev => prev.filter(tag => tag.id !== pk))
+        })
+        .catch(error => console.log(error));
+    }
+
+    const [tags, setTags] = useState<Tag[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/tags/")
+        .then((res) => res.json())
+        .then((data) => setTags(data))
+        .catch((error) => console.error(error))
+    }, [])
+
+
+    return(
+        <div className="space-y-12">
+            <h2 className="text-white text-xl font-bold">Tag list</h2>
+
+            <div className="flex flex-col gap-10">
+                {tags.map((tag: any) => (
+                    <div key={tag.id} className="flex justify-between outline-white/20 outline-1 outline-offset-2 px-4 py-3 rounded-md bg-gray-800">
+                        <div className="flex gap-10">
+                            <p>Id: {tag.id}</p>
+                            <p>Name: {tag.name}</p>
+                        </div>
+                        <div className="flex gap-5">
+                            <button className="px-2 py-1 rounded-md bg-sky-600 font-bold text-sm">Atualizar</button>
+                            <button onClick={() => deleteTag(tag.id)} className="px-2 py-1 rounded-md bg-red-600 font-bold text-sm">Excluir</button>
+                        </div>
+
+                    </div>
+                ))}
+            </div>
+        </div>      
+    )
+}
