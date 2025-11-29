8d0f665e85e5645361b7cbb7890590dcbf9780c5
Guilherme Lira <guilherme.liratsouza@gmail.com>
2025-11-29 10:16:19 -0300

    refactor(header): minor change

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -4,6 +4,7 @@ import {
     Heart,
 } from "lucide-react"
 import HeaderNavButton from "./HeaderNavButton"
+import Link from "next/link"
 
 export default function Header(){
 
@@ -12,7 +13,9 @@ export default function Header(){
         <div className="
         fixed top-0 flex items-center justify-between align-center px-20 w-full h-16 bg-neutral-900 text-white z-1000">
             <nav className="flex justify-center">
-                <button className="text-3xl mr-20">BRESHOP</button>
+                <Link href={'/'} className="text-3xl mr-20">
+                    BRESHOP
+                </Link>
                 <HeaderNavButton>HOMEM</HeaderNavButton>  
                 <HeaderNavButton>MULHER</HeaderNavButton>  
                 <HeaderNavButton>CASA</HeaderNavButton>  
@@ -22,9 +25,9 @@ export default function Header(){
                     <Search size={20}/>
                 </button>
                 
-                <button>
+                <Link href={'/admin'}>
                     <UserRound size={20}/>
-                </button>
+                </Link>
 
                 <button>
                     <Heart size={20}/>
